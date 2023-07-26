17317a022a2b28c7f0c1630633305b04a69bf1e4
150030211-myseneca <133421093+150030211-myseneca@users.noreply.github.com>
2023-07-26 10:04:24 -0400

    Update preprocess-iot-monitor-customdata-otics.py

--- TML Crash course/preprocess-iot-monitor-customdata-otics.py
+++ TML Crash course/preprocess-iot-monitor-customdata-otics.py
@@ -190,7 +190,7 @@ latlong=lat:long'
  
      identifier = "IoT device performance and failures"
 
-     preprocesslogic='anomprob,trend,avg'
+     preprocesslogic='max,min,sum'
 
      
 #     pathtotmlattrs='oem=id,lat=subject.reference,long=component.0.code.coding.0.display,location=component.1.valueQuantity.value'     
